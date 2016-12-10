569e7d209cb7656fa7f9da5c87020533ca9f9f42
achyu <achyu@10.6.7.119>
2016-12-10 22:02:57 +0530

    user prof

--- src/UserProf.java
+++ src/UserProf.java
@@ -15,6 +15,8 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class UserProf extends JFrame {
 
@@ -41,93 +43,38 @@ public class UserProf extends JFrame {
 	 */
 	public UserProf() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 842, 468);
+		setBounds(100, 100, 573, 327);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
-		JLabel lblProfilePicture = new JLabel("Profile Picture");
-		lblProfilePicture.setHorizontalAlignment(SwingConstants.CENTER);
-		lblProfilePicture.setBorder(border);
 		
 		JLabel lblName = new JLabel("Name :");
-		lblName.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		
-		JLabel lblDob = new JLabel("DOB :");
-		lblDob.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		
-		JLabel lblMissionAssigned = new JLabel("Mission Assigned :");
-		lblMissionAssigned.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblName.setBounds(15, 16, 143, 57);
+		lblName.setFont(new Font("Tahoma", Font.BOLD, 23));
 		
 		JLabel lblRank = new JLabel("Rank :");
-		lblRank.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblRank.setBounds(15, 79, 90, 17);
+		lblRank.setFont(new Font("Tahoma", Font.BOLD, 23));
 		
-		JButton btnMissionReport = new JButton("Mission Report");
+		JButton btnMissionReport = new JButton("(informer report)");
+		btnMissionReport.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnMissionReport.setBounds(72, 190, 113, 23);
 		
-		JButton btnReportStatus = new JButton("Report Status");
+		JButton btnReportStatus = new JButton("Check  Status");
+		btnReportStatus.setBounds(191, 190, 99, 23);
 		
-		JButton btnBack = new JButton("Back");
-		
-		JButton btnNewButton = new JButton("New Mission");
-		
-		JTextArea txtrMissionDescription = new JTextArea();
-		txtrMissionDescription.setLineWrap(true);
-		txtrMissionDescription.setText("Mission Description");
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblProfilePicture, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblName, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblDob, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblRank, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblMissionAssigned, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE)))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-									.addComponent(btnBack, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(btnMissionReport, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(btnReportStatus)
-									.addGap(392))
-								.addComponent(txtrMissionDescription, GroupLayout.DEFAULT_SIZE, 687, Short.MAX_VALUE))))
-					.addContainerGap())
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblName)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(lblDob, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(lblRank, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(lblMissionAssigned, GroupLayout.PREFERRED_SIZE, 17, GroupLayout.PREFERRED_SIZE))
-						.addComponent(lblProfilePicture, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE))
-					.addGap(43)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnMissionReport)
-						.addComponent(btnReportStatus))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(txtrMissionDescription, GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE)
-						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 148, Short.MAX_VALUE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnBack)
-					.addContainerGap())
-		);
-		contentPane.setLayout(gl_contentPane);
+		JButton btnBack = new JButton("Log Out");
+		btnBack.setBounds(314, 190, 113, 23);
+		contentPane.setLayout(null);
+		contentPane.add(btnBack);
+		contentPane.add(btnMissionReport);
+		contentPane.add(btnReportStatus);
+		contentPane.add(lblRank);
+		contentPane.add(lblName);
 	}
 }
