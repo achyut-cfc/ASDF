ad0d180dcefe03c12d2ae1eb11d5504c3c66b317
achyu <achyu@10.6.7.119>
2016-12-11 09:55:35 +0530

    exec inb

--- src/Executive_inbox.java
+++ src/Executive_inbox.java
@@ -13,10 +13,13 @@ import javax.swing.AbstractListModel;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Executive_inbox extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -39,7 +42,7 @@ public class Executive_inbox extends JFrame {
 	 */
 	public Executive_inbox() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(260, 60, 700, 550);
+		setBounds(260, 60, 658, 447);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -48,30 +51,27 @@ public class Executive_inbox extends JFrame {
 		JLabel lblNewLabel = new JLabel("INBOX");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel.setBounds(271, 24, 120, 53);
+		lblNewLabel.setBounds(261, 24, 120, 53);
 		contentPane.add(lblNewLabel);
 		
-		JList list = new JList();
-		
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		list.setBounds(125, 88, 418, 357);
-		contentPane.add(list);
-		
 		JButton btnView = new JButton("VIEW");
-		btnView.setBounds(471, 465, 89, 23);
+		btnView.setBounds(387, 369, 89, 23);
 		contentPane.add(btnView);
 		
 		JButton btnNewButton = new JButton("Back");
-		btnNewButton.setBounds(570, 465, 89, 23);
+		btnNewButton.setBounds(486, 369, 89, 23);
 		contentPane.add(btnNewButton);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"001"},
+			},
+			new String[] {
+				"Case Number"
+			}
+		));
+		table.setBounds(99, 80, 485, 259);
+		contentPane.add(table);
 	}
-
 }
